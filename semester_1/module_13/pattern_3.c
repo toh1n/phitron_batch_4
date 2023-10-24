0f377a4097ac3e026d79f1e9e00c977991ff6203
toh1n <tohin214@gmail.com>
2023-10-25 04:52:49 +0600

    module_13

---
+++ semester_1/module_13/pattern_3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,s,k;
+    scanf("%d",&n);
+    s = n-1;
+    k = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <s; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < k; j++)
+        {
+            printf("*");
+        }
+        s--;
+        k = k + 2 ;
+        
+        printf("\n");
+        
+    }
+
+    s = 0;
+    k = 2 * n - 1;
+
+    for (int i = n ; i > 0; i--)
+    {
+        for (int j = s; j > 0; j--)
+        {
+            printf(" ");
+        }
+        for (int j = k; j > 0; j--)
+        {
+            printf("*");
+        }
+        s++;
+        k = k - 2 ;
+        
+        printf("\n");
+        
+    }
+    
+}
